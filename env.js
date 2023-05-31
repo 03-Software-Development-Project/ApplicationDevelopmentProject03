e1474512d01c2bd46ad8c5505708c02ac48d963c
Lâm Phạm <plam84524@gmail.com>
2023-05-31 11:17:51 +0700

    added IP for more flexible code

--- env.js
+++ env.js
@@ -1,8 +1,9 @@
 const PORT = '3001';
-const API_LOGIN = `http://192.168.100.26:${PORT}/api/data`;
-
+const IP = '192.168.100.6';
+const API_LOGIN = `http://${IP}:${PORT}/api/data`;
 module.exports = {
   API_LOGIN,
   PORT,
+  IP,
   // Add more exported variables here
 };
