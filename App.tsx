aa175517eba1db5e60ef7f7cb2e4643f6fb67b78
Lâm Phạm <plam84524@gmail.com>
2023-06-01 16:07:31 +0700

    Added demo function for stack navigation

--- App.tsx
+++ App.tsx
@@ -8,41 +8,52 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import SignIn from './src/screen/signIn';
+import SignIn from './src/screen/signIn/signIn';
 import Home from './src/screen/home/home';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
-// const Stack = createStackNavigator();
-// const AppStack = () => (
-//   <Stack.Navigator>
-//     <Stack.Screen name="Login" component={LoginScreen} />
-//     {/* Add other screens to the stack if needed */}
-//     {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-//   </Stack.Navigator>
-// );
-
-const Stack = createNativeStackNavigator();
-
-function MyStack() {
+function HomeScreen({navigation}) {
   return (
-    <NavigationContainer>
-      <SignIn />
-    </NavigationContainer>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
   );
 }
 
+const Stack = createNativeStackNavigator();
+
 function App(): JSX.Element {
   return (
-    <View style={styles.root}>
-      <MyStack />
-    </View>
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen name="Login" component={SignIn} />
-    //     {/* Add other screens to the stack if needed */}
-    //     <Stack.Screen name="Home" component={Home} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    // <View style={styles.root}>
+    //   <SignIn />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
