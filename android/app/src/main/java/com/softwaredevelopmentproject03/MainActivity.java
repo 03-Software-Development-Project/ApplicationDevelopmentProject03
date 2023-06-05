0d37536d9555fc0068161871b33335e7c6d63619
Lâm Phạm <plam84524@gmail.com>
2023-06-05 14:14:37 +0700

    aaded validator, native screens packages

--- android/app/src/main/java/com/softwaredevelopmentproject03/MainActivity.java
+++ android/app/src/main/java/com/softwaredevelopmentproject03/MainActivity.java
@@ -4,7 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-
+import android.os.Bundle;
 public class MainActivity extends ReactActivity {
   @Override
     protected void onCreate(Bundle savedInstanceState) {
