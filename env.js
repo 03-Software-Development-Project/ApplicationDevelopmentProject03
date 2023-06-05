22648d430f059d01bfac9250740b9f275fe0b09c
Lâm Phạm <plam84524@gmail.com>
2023-06-05 17:17:01 +0700

    added question token to get api

--- env.js
+++ env.js
@@ -1,9 +1,11 @@
 const PORT = '3001';
 const IP = '10.7.136.21';
 const API_LOGIN = `http://${IP}:${PORT}/api/data`;
+const API_QUESTION_TOKEN = '6Ds4vq9V7cZES7I3YTxl9KapdwFZa04X47fvIX4s';
 module.exports = {
   API_LOGIN,
   PORT,
   IP,
+  API_QUESTION_TOKEN,
   // Add more exported variables here
 };
