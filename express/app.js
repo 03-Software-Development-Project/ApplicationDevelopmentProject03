7271d85ac187bf31d220db4b82e7d652cf3de4ba
Lâm Phạm <plam84524@gmail.com>
2023-06-05 17:14:39 +0700

    delete old api. Added api to get data from quizapi

--- express/app.js
+++ express/app.js
@@ -1,125 +1,44 @@
 const express = require('express');
 const app = express();
-const admin = require('firebase-admin');
-const serviceAccount = require('./react-native-project-5704c-firebase-adminsdk-zp6q5-d1c17a79a8.json'); // Path to the private key file
-const {PORT, IP} = require('../env');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-const db = admin.firestore();
+const axios = require('axios');
+const {PORT, IP, API_QUESTION_TOKEN} = require('../env');
+const {saveDataAsJSON, transformData} = require('./fileUtils');
+app.get('/api/questions', async (req, res) => {
+  /* This code block is defining the API endpoint URL and a set of parameters for making an HTTP GET
+request to the Quiz API. The `apiUrl` variable is set to the URL of the Quiz API endpoint for
+retrieving questions. The `params` object contains two properties: `apiKey` and `limit`. The
+`apiKey` property is set to the value of the `API_QUESTION_TOKEN` environment variable, which is
+used to authenticate the request to the Quiz API. The `limit` property is set to 10, which specifies
+the maximum number of questions to retrieve from the API. These parameters are passed as options to
+the `axios.get()` method to make the HTTP GET request to the Quiz API endpoint. */
+  const apiUrl = 'https://quizapi.io/api/v1/questions';
+  const params = {
+    apiKey: API_QUESTION_TOKEN,
+    limit: 10,
+  };
 
-app.use(express.json());
-
-/* ANCHOR This code defines a route for the Express app that listens for GET requests to the '/api/data/guide'
-endpoint. When a request is received, it retrieves data from the 'guide' collection in Firestore and
-adds it to an array called 'data'. For each document in the 'guide' collection, it also retrieves
-all the documents in the 'rate' subcollection and adds them to an array called 'rates'. The 'rates'
-array is then added to the 'documentData' object for the current document in the 'guide' collection.
-Finally, the 'documentData' object is added to the 'data' array. The 'data' array is then sent as a
-JSON response to the client. If there is an error retrieving the data, a 500 error response is sent. */
-app.get('/api/data/guide', async (req, res) => {
+  /* This code block is making an asynchronous HTTP GET request to an API endpoint using the axios
+ library. It is passing in the API endpoint URL and a set of parameters as options for the request.
+ If the request is successful, it sends the response data back to the client as a JSON object. If
+ the request fails, it catches the error and logs it to the console, then sends an error response to
+ the client with a 500 status code and an error message. This is a common pattern for handling
+ errors in asynchronous code. */
   try {
-    const collectionRef = db.collection('guide');
-    const snapshot = await collectionRef.get();
-
-    const data = [];
-    for (const doc of snapshot.docs) {
-      const documentData = doc.data();
-
-      console.log(documentData);
-
-      const ratingRef = collectionRef.doc(doc.id).collection('rate');
-      const ratingSnapshot = await ratingRef.get();
-
-      const rates = [];
-      /* ANCHOR `ratingSnapshot.forEach(rateDoc => { rates.push(rateDoc.data()); });` is iterating over each
-      document in the `rate` subcollection of the current `guide` document and pushing its data to
-      the `rates` array. This is done to retrieve all the ratings associated with the current
-      `guide` document and add them to the `documentData` object before pushing it to the `data`
-      array. */
-      ratingSnapshot.forEach(rateDoc => {
-        rates.push(rateDoc.data());
-      });
-
-      documentData.rate = rates;
-      data.push(documentData);
-
-      console.log(
-        `snapshot.size: ${snapshot.size} | data.length: ${data.length}`,
-      );
-    }
-
-    res.json(data);
+    const response = await axios.get(apiUrl, {params});
+    res.json(response.data);
+    const transformedData = transformData(response.data);
+    saveDataAsJSON(transformedData, './data/questions.json');
   } catch (error) {
-    console.error('Error retrieving data:', error);
-    res.status(500).json({error: 'Failed to retrieve data.'});
-  }
-});
-
-/* ANCHOR This code defines a route for the Express app that listens for GET requests to the '/api/data'
-endpoint. When a request is received, it retrieves data from the Firestore database in the 'testing'
-collection and adds it to an array called 'data'. For each document in the 'testing' collection, it
-also retrieves all the documents in the 'comment' subcollection and adds them to an array called
-'comments'. The 'comments' array is then added to the 'documentData' object for the current document
-in the 'testing' collection. Finally, the 'documentData' object is added to the 'data' array. The
-'data' array is then sent as a JSON response to the client. If there is an error retrieving the
-data, a 500 error response is sent. */
-app.get('/api/data', async (req, res) => {
-  try {
-    const collectionRef = db.collection('testing'); // Replace with the name of your collection
-    const snapshot = await collectionRef.get();
-
-    const data = [];
-    for (const doc of snapshot.docs) {
-      const documentData = doc.data();
-
-      const commentRef = collectionRef.doc(doc.id).collection('comment');
-      const commentSnapshot = await commentRef.get();
-
-      const comments = [];
-      commentSnapshot.forEach(commentDoc => {
-        comments.push(commentDoc.data());
-      });
-
-      documentData.comments = comments;
-      data.push(documentData);
-    }
-
-    res.json(data);
-  } catch (error) {
-    console.error('Error retrieving data:', error);
-    res.status(500).json({error: 'Failed to retrieve data.'});
-  }
-});
-
-// Example route to add data to Firestore
-app.post('/api/data', (req, res) => {
-  const collectionRef = db.collection('testing'); // Replace with the name of your collection
-  const newData = req.body;
-
-  // Ensure newData is a valid JavaScript object
-  if (
-    typeof newData === 'object' &&
-    !Array.isArray(newData) &&
-    newData !== null
-  ) {
-    collectionRef
-      .add(newData)
-      .then(() => {
-        res.status(200).json({message: 'Data added successfully.'});
-      })
-      .catch(error => {
-        console.error('Error adding data: ', error);
-        res.status(500).json({error: 'Failed to add data.'});
-      });
-  } else {
-    res.status(400).json({error: 'Invalid data format.'});
+    console.error(error);
+    res
+      .status(500)
+      .json({error: 'An error occurred while fetching the questions.'});
   }
 });
 
 // Start the Express server
 app.listen(PORT, () => {
-  console.log(`Express app listening on http://${IP}:${PORT}/api/data`);
+  console.log(`Express app listening on http://${IP}:${PORT}/api/questions`);
 });
 
 module.exports = app;
