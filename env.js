835f613b0dcc63be8d97cb82573dff8092465aa6
Lâm Phạm <plam84524@gmail.com>
2023-06-06 15:52:20 +0700

    added question limit constant

--- env.js
+++ env.js
@@ -1,11 +1,13 @@
 const PORT = '3001';
-const IP = '10.7.136.21';
+const IP = '192.168.0.38';
 const API_LOGIN = `http://${IP}:${PORT}/api/data`;
+const QUESTION_LIMIT = 2;
 const API_QUESTION_TOKEN = '6Ds4vq9V7cZES7I3YTxl9KapdwFZa04X47fvIX4s';
 module.exports = {
   API_LOGIN,
   PORT,
   IP,
   API_QUESTION_TOKEN,
+  QUESTION_LIMIT,
   // Add more exported variables here
 };
