67a6fb4dda70369c5505014da9505238d0de2ef2
Lâm Phạm <plam84524@gmail.com>
2023-06-14 16:01:52 +0700

    added POINTS and  DESIREDDIFFICULTY variable

--- env.js
+++ env.js
@@ -1,13 +1,25 @@
 const PORT = '3001';
-const IP = '192.168.0.38';
+const IP = '192.168.6.222';
 const API_LOGIN = `http://${IP}:${PORT}/api/data`;
-const QUESTION_LIMIT = 2;
+const QUESTION_LIMIT = 5;
 const API_QUESTION_TOKEN = '6Ds4vq9V7cZES7I3YTxl9KapdwFZa04X47fvIX4s';
+const POINTS = {
+  easy: 3,
+  medium: 4,
+  hard: 5,
+};
+const DESIREDDIFFICULTY = {
+  easy: 'easy',
+  medium: 'medium',
+  hard: 'hard',
+};
 module.exports = {
   API_LOGIN,
   PORT,
   IP,
   API_QUESTION_TOKEN,
   QUESTION_LIMIT,
+  POINTS,
+  DESIREDDIFFICULTY,
   // Add more exported variables here
 };
