e8d2d487f33563d132f3db76c2d23624c11947b8
Phuoc Hai <phuochai.work@gmail.com>
2023-06-15 22:51:50 +0700

    new changes in UI

---
+++ constants/color.js
@@ -0,0 +1,67 @@
+export default {
+  // Greyscale Color , used for text
+  'Greyscale.50': '#F9FAFB',
+  'Greyscale.100': '#F3F4F6',
+  'Greyscale.200': '#E5E7EB',
+  'Greyscale.300': '#D1D5DB',
+  'Greyscale.400': '#9CA3AF',
+  'Greyscale.500': '#6B7280',
+  'Greyscale.600': '#4B5563',
+  'Greyscale.700': '#374151',
+  'Greyscale.800': '#1D2734',
+  'Greyscale.900': '#111827',
+  // Primary/ Brand Color
+  'Primary.100': '#D0E3FD',
+  'Primary.200': '#A0C4FA',
+  'Primary.300': '#78A2F9',
+  'Primary.400': '#5585F4',
+  'Primary.500': '#2058ED',
+  'Primary.600': '#1743CB',
+  'Primary.700': '#1031AA',
+  'Primary.800': '#0A2289',
+  'Primary.900': '#061771',
+
+  // Success  Color
+  'Success.100': '#E1FAD6',
+  'Success.200': '#BFF5AE',
+  'Success.300': '#8EE380',
+  'Success.400': '#61C75B',
+  'Success.500': '#2DA331',
+  'Success.600': '#208C2E',
+  'Success.700': '#16752A',
+  'Success.800': '#0E5E26',
+  'Success.900': '#084E23',
+
+  // Info Color
+  'info.100': '#D0EDFE',
+  'info.200': '#A3D8FD',
+  'info.300': '#74BDFB',
+  'info.400': '#51A3F7',
+  'info.500': '#1A7BF2',
+  'info.600': '#135FD0',
+  'info.700': '#0D46AE',
+  'info.800': '#08318C',
+  'info.900': '#042274',
+
+  // Warning Color
+  'Warning.100': '#FEF4CB',
+  'Warning.200': '#FDE798',
+  'Warning.300': '#FBD564',
+  'Warning.400': '#F7C23D',
+  'Warning.500': '#F2A500',
+  'Warning.600': '#D08700',
+  'Warning.700': '#AE6B00',
+  'Warning.800': '#8C5100',
+  'Warning.900': '#744000',
+
+  // Danger Color
+  'Danger.100': '#FEE4D2',
+  'Danger.200': '#FEC2A5',
+  'Danger.300': '#FD9978',
+  'Danger.400': '#FB7257',
+  'Danger.500': '#F93220',
+  'Danger.600': '#D61717',
+  'Danger.700': '#B3101D',
+  'Danger.800': '#900A20',
+  'Danger.900': '#770621',
+}
---
+++ constants/index.js
@@ -0,0 +1,4 @@
+import typography from './typography'
+import color from './color'
+
+export {typography, color}
